bb8e22ec009617cfbb2c1f5a6c4fc58ea4df6701
dce23-github <dhirenchugh23@gmail.com>
2022-01-09 18:44:41 +0530

    Add report

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
--- public/js/Char.js
+++ public/js/Char.js
@@ -1,7 +1,7 @@
 const Identifier = require("./Identifier");
 
-class Char{
-    constructor(siteId ,position, value, counter){
+class Char {
+    constructor(siteId, position, value, counter) {
         this.position = position;
         this.siteId = siteId;
         this.value = value;
@@ -10,20 +10,20 @@ class Char{
 
 
 
-    compareChar(ch){
+    compareChar(ch) {
         const pos1 = this.position;
         const pos2 = ch.position;
 
-        for(let i = 0;i < Math.min(pos1.length, pos2.length); i++){
+        for (let i = 0; i < Math.min(pos1.length, pos2.length); i++) {
             const id1 = pos1[i];
             const id2 = pos2[i];
             const temp = new Identifier(id1.digit, id1.siteId);
             const res = temp.compareIdentifier(id2);
-            if(res !== 0) return res;
+            if (res !== 0) return res;
         }
 
-        if(pos1.length < pos2.length) return -1;
-        else if(pos1.length > pos2.length) return 1;
+        if (pos1.length < pos2.length) return -1;
+        else if (pos1.length > pos2.length) return 1;
         else return 0;
     }
 }
